9d35e304621c4e1b5b1cd934da7a0b8ae6b84629
Sebastian Bularca <sebastian@tocaboca.com>
2018-09-10 16:42:31 +0200

    added backtracking (unfinished)

---
+++ ClassicAlgorythms/BinarySieve.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClassicAlgorithms
+{
+    public class BinarySieve
+    {
+        public static void ExecuteBinaryAndSieve()
+        {
+            BinarySieve sieve = new BinarySieve();
+
+            int maxPrimeSetSize;
+
+            if (!int.TryParse(Console.ReadLine(), out maxPrimeSetSize))
+            {
+                Console.WriteLine("Not a number. Bhye, Bhye!");
+                Console.ReadLine();
+                return;
+            }
+            if (maxPrimeSetSize > 500000)
+            {
+                Console.WriteLine("The number was larger than 500.000. We don't have all day here...");
+                Console.ReadLine();
+                return;
+            }
+            DateTime startTime = DateTime.Now;
+            sieve.BinarySearch(sieve.SieveOfEratostenes(maxPrimeSetSize));
+            DateTime endTime = DateTime.Now;
+            string deltaTime = (endTime - startTime).TotalSeconds.ToString();
+            Console.WriteLine($"\nExecution time was {deltaTime} seconds");
+            Console.ReadLine();
+        }
+
+        //private static void FlushKeyboard()
+        //{
+        //    while (Console.In.Peek() != -1)
+        //        Console.In.ReadLine();
+        //}
+
+        /// <summary>
+        /// Simple binary search
+        /// </summary>
+        /// <param name="listOfNumbers"></param>
+        private void BinarySearch(List<long> listOfNumbers)
+        {
+            List<long> set = new List<long>();
+            bool solved = false;
+            set = listOfNumbers;
+
+            Console.WriteLine($"Max number of searches should be: {(int)Math.Log(set.Count, 2) + 1}");
+
+            Random rnd = new Random();
+            int elem = rnd.Next(0, listOfNumbers.Count);
+
+            int min = 0, max = set.Count - 1;
+
+            if (elem < set[min] || elem > set[max])
+            {
+                Console.WriteLine($"Element {elem} does not exist in the array");
+                Console.ReadLine();
+            }
+
+            int counter = 0;
+
+            while (min < max && !solved)
+            {
+                int avg = (int)Math.Floor(((decimal)max + (decimal)min) / 2);
+
+                if (elem == set[avg])
+                {
+                    Console.WriteLine($"Element {elem} was found at postion {avg}");
+                    Console.WriteLine($"Number of iterations: {counter}");
+                    Console.ReadLine();
+                    solved = true;
+                }
+                else
+                {
+                    if (elem < set[avg])
+                        max = avg - 1;
+                    else
+                        min = avg + 1;
+                }
+
+                counter++;
+            }
+
+            Console.WriteLine($"Element {elem} does not exist in the array");
+            Console.WriteLine($"Number of iterations: {counter}");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Finds all the prime numbers up to maxNumber.
+        /// If the number is larger than n, an out of memory exception will be thrown, based on the current available memory.
+        /// </summary>
+        /// <param name="maxNumber"></param>
+        /// <returns></returns>
+        private List<long> SieveOfEratostenes(int maxNumber)
+        {
+            Int64[] result = new Int64[maxNumber + 1];
+            HashSet<Int64> nonPrimes = new HashSet<Int64>();
+
+            for (Int64 i = 2; i < maxNumber + 1; i++)
+                result[i - 2] = i;
+
+            for (Int64 i = 0; i < result.Length; i++)
+            {
+
+                int p = 2;
+                if (!nonPrimes.Contains(result[i]))
+                {
+                    while (p <= maxNumber)
+                    {
+                        Int64 x = result[i] * p;
+                        p++;
+
+                        if (x <= maxNumber && !nonPrimes.Contains(x))
+                        {
+                            nonPrimes.Add(x);
+                        }
+                    }
+                }
+            }
+
+            var finalResult = result.Except(nonPrimes);
+
+            Console.WriteLine();
+
+            foreach (int elem in finalResult)
+            {
+                Console.Write(elem + ", ");
+            }
+            Console.ReadLine();
+            return finalResult.ToList();
+        }
+    }
+}
---
+++ ClassicAlgorythms/EightQueenProblem.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace ClassicAlgorithms
+{
+    public class EightQueenProblem
+    {
+        public EightQueenProblem(int N)
+        {
+            n = N;
+        }
+
+        private int n;
+        private int[,] board;
+
+        public static void Backtracking()
+        {
+            //TODO: Add input for number of rows and collumns
+
+            EightQueenProblem eightQueenProblem = new EightQueenProblem(8);
+
+            eightQueenProblem.Initialize();
+
+            eightQueenProblem.DisplaySolution();
+
+            Console.ReadLine();
+        }
+
+        private void Initialize()
+        {
+            board = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    board[i, j] = 0;
+                }
+            }
+        }
+
+        private void DisplaySolution()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (!SolutionFinder(i))
+                    return;
+            }
+
+
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(board[i, j] + (j < n - 1?", ": ""));
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private bool CheckPlacement(int row, int col)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (board[i, col] == 1) return false;
+                if (board[col, i] == 1) return false;
+            }
+
+            for (int i = row, j = col; i >=0 && j >= 0; i--, j--)
+            {
+                if (board[i, j] == 1) return false;
+            }
+
+            for (int i = row, j = n; i >= 0 && j < n; i--, j++)
+            {
+                if (board[i, j] == 1) return false;
+            }
+
+            return true;
+        }
+        private bool SolutionFinder(int row)
+        {
+            if (row >= n) return true;
+
+            for (int i = 0; i < n; i++)
+            {
+                    if (CheckPlacement(row, i))
+                    {
+                        board[row, i] = 1;
+                        if (SolutionFinder(row + 1))  return true;
+                    }
+            }
+            return false;
+        }
+    }
+}

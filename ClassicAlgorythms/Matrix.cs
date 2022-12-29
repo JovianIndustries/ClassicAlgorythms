c3d160143b6ce0464823030a4dcad021e0c65221
uzihead <sebastian.bularca@gmail.com>
2022-12-29 16:05:23 +0100

    added sine chargpt generated algorythms

---
+++ ClassicAlgorythms/Matrix.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace MatrixMultiplication {
+	public class Matrix {
+		static void Test(string[] args)
+		{
+			int[,] matrixA = { { 1, 2 }, { 3, 4 } };
+			int[,] matrixB = { { 5, 6 }, { 7, 8 } };
+
+			int[,] result = MultiplyMatrices(matrixA, matrixB);
+
+			for (int i = 0; i < result.GetLength(0); i++)
+			{
+				for (int j = 0; j < result.GetLength(1); j++)
+				{
+					Console.Write(result[i, j] + " ");
+				}
+
+				Console.WriteLine();
+			}
+		}
+
+		public static int[,] MultiplyMatrices(int[,] matrixA, int[,] matrixB)
+		{
+			int rowsA = matrixA.GetLength(0);
+			int colsA = matrixA.GetLength(1);
+			int rowsB = matrixB.GetLength(0);
+			int colsB = matrixB.GetLength(1);
+
+			if (colsA != rowsB)
+			{
+				throw new InvalidOperationException("The matrices cannot be multiplied.");
+			}
+
+			int[,] result = new int[rowsA, colsB];
+
+			for (int i = 0; i < rowsA; i++)
+			{
+				for (int j = 0; j < colsB; j++)
+				{
+					for (int k = 0; k < colsA; k++)
+					{
+						result[i, j] += matrixA[i, k] * matrixB[k, j];
+					}
+				}
+			}
+
+			return result;
+		}
+		
+		public static int[,] TransposeMatrix(int[,] matrix)
+		{
+			int rows = matrix.GetLength(0);
+			int cols = matrix.GetLength(1);
+
+			int[,] transposed = new int[cols, rows];
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					transposed[j, i] = matrix[i, j];
+				}
+			}
+
+			return transposed;
+		}
+		
+	}
+}
---
+++ ClassicAlgorythms/Sort.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace ClassicAlgorithms {
+	public static class Sort {
+		public static void QuickSort(int[] array, int left, int right) {
+			if (left < right) {
+				int pivotIndex = Partition(array, left, right);
+
+				QuickSort(array, left, pivotIndex - 1);
+				QuickSort(array, pivotIndex + 1, right);
+			}
+		}
+
+		private static int Partition(int[] array, int left, int right) {
+			int pivot = array[right];
+			int i = left;
+
+			for (int j = left; j < right; j++) {
+				if (array[j] <= pivot) {
+					Swap(array, i, j);
+					i++;
+				}
+			}
+
+			Swap(array, i, right);
+
+			return i;
+		}
+
+		private static void Swap(int[] array, int i, int j) {
+			(array[i], array[j]) = (array[j], array[i]);
+		}
+
+
+		public static void MergeSort(int[] array) {
+			if (array.Length > 1) {
+				int mid = array.Length / 2;
+				int[] left = new int[mid];
+				int[] right = new int[array.Length - mid];
+
+				Array.Copy(array, left, mid);
+				Array.Copy(array, mid, right, 0, right.Length);
+
+				MergeSort(left);
+				MergeSort(right);
+
+				Merge(array, left, right);
+			}
+		}
+
+		private static void Merge(int[] array, int[] left, int[] right) {
+			int i = 0;
+			int j = 0;
+			int k = 0;
+
+			while (i < left.Length && j < right.Length) {
+				if (left[i] <= right[j]) {
+					array[k] = left[i];
+					i++;
+				}
+				else {
+					array[k] = right[j];
+					j++;
+				}
+
+				k++;
+			}
+
+			while (i < left.Length) {
+				array[k] = left[i];
+				i++;
+				k++;
+			}
+
+			while (j < right.Length) {
+				array[k] = right[j];
+				j++;
+				k++;
+			}
+		}
+	}
+}
